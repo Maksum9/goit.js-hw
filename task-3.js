02a331cbd2aaf72b99568a8fcc97f43798d55bfb
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-10-15 11:48:01 +0300

    up hw 01

---
+++ task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content,padding,border) {
+const contentWidth = parseFloat(content)
+const contentPadding = parseFloat(padding)
+const contentBorder = parseFloat(border)
+return contentWidth + 2 * contentPadding + 2 * contentBorder
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
